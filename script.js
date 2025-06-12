8cf0268d1a6c74f6220040caae338bc4b7cdfc5c
Vinícius Cesar Machado Cardoso <49768215879@escola927.edu>
2025-06-12 08:11:58 -0300

    Estilizado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // definir constantes para uso no código
 const btnAdd = document.getElementById("btn-add");
 const tarefa = document.getElementById('tarefa');
@@ -7,9 +8,40 @@ const titulo = document.getElementById('titulo');
 let nome = prompt("Qual seu nome?")
 titulo.innerHTML = ` Lista de tarefa: ${nome} `;
 
-
+//acoompanha o evento de clique do botão adicionar tarefa 
 btnAdd.addEventListener("click", criaTarefa);
 
 function criaTarefa(){
-    
+
+    const listItem = document.createElement('li');
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem);
+
+    //criar botão de deletar tarefa 
+    const removeButton = document.createElement('button');
+    removeButton.id = "remove";
+    removeButton.textContent = "👎";
+   
+    //criar botão de concluir tarefa 
+    const concluirButton = document.createElement('button');
+     concluirButton.id = "conclui";
+     concluirButton.textContent = "👍";
+ 
+     
+ let buttonsItem = document.createElement('div');
+ buttonsItem.classList.toggle('buttonItem')
+ listItem.appendChild(buttonsItem);
+ buttonsItem.appendChild(concluirButton);
+ buttonsItem.appendChild(removeButton);
+
+ concluirButton.addEventListener("click", function(){
+    listItem.classList.toggle('completed')
+   taskList.removeAttribute(buttonsItem)
+   this.textContent = "✅"
+ })
+ removeButton.addEventListener("click", function(){
+    taskList.removeChild(listItem);
+})
+
 }
+
